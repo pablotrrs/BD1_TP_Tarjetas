63e17df3eb105099e2fae25950677a7e34dd6e85
Matias Fretes <maty.fretes10@gmail.com>
2020-12-03 00:35:10 -0300

    Cambios en testResumen

--- sql/testResumen.go
+++ sql/testResumen.go
@@ -7,8 +7,7 @@ import(
 func testGenResumen(){
 	
 	_, err = db.Query(`
-	
-	SELECT generarresumen(14348789, 11, 2020); 
+
 	SELECT generarresumen(14348789, 12, 2020); 
 	SELECT generarresumen(12228777, 12, 2020);
 	SELECT generarresumen(11448979, 12, 2020);  
@@ -69,13 +68,20 @@ func testResultDetalle(){
 			
 			SELECT * INTO cabeceraDEC FROM cabecera WHERE nrotarjeta = '4003300224374894';
 			SELECT * INTO detalleDEC FROM detalle WHERE nroresumen = cabeceraDEC.nroresumen;
-			SELECT * INTO cierreTarjetaDEC FROM cierre WHERE terminacion = 4 AND mes = 11;
+			SELECT * INTO cierreTarjetaDEC FROM cierre WHERE terminacion = 4 AND mes = 12;
 			
 			PERFORM * FROM compra WHERE nrotarjeta = cabeceraDEC.nrotarjeta AND cabeceraDEC.nroresumen = detalleDEC.nroresumen AND fecha::date >=  cierreTarjetaDEC.fechainicio AND fecha::date <= cierreTarjetaDEC.fechacierre AND monto = detalleDEC.monto;
 			IF not found THEN
 				ret := ret and false;				
 			END IF;	
 			
+			----Persona que tiene todo pagado---
+			
+			SELECT * INTO cabecera2DEC FROM cabecera WHERE nrotarjeta = '4032002134557009';
+			PERFORM * FROM compra WHERE nrotarjeta = '4032002134557009' AND pagado = true AND cabecera2DEC.total = 0;
+			IF not found THEN
+				ret := ret and false;				
+			END IF;	
 			
 			return ret;
 			
